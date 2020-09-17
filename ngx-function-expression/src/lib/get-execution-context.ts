34a451327b474c8061d9a446d1ab0bda2a1b394f
Nico Rausch <rausch.nico@gmail.com>
2020-09-17 13:33:45 +0200

    Updated context resolver

--- ngx-function-expression/src/lib/get-execution-context.ts
+++ ngx-function-expression/src/lib/get-execution-context.ts
@@ -2,5 +2,5 @@ import {ChangeDetectorRef} from '@angular/core';
 
 export function getExecutionContext(cdRef: ChangeDetectorRef): object {
   // tslint:disable-next-line:no-string-literal
-  return cdRef['_lView']?.[8] ?? cdRef['_view']?.['context'];
+  return cdRef['context'] ?? cdRef['_lView']?.[8] ?? cdRef['_view']?.['context'];
 }
--- ngx-function-expression/src/test/multi-level-fn-call.pipe.spec.ts
+++ ngx-function-expression/src/test/multi-level-fn-call.pipe.spec.ts
@@ -1,13 +1,20 @@
-import {Component, Input, ViewChild} from "@angular/core";
-import {FunctionExpression} from "../lib/fn-function-expression.type";
+import {ChangeDetectionStrategy, Component, Input, ViewChild} from "@angular/core";
 import {TestBed} from "@angular/core/testing";
 import {FnCallPipe} from "../lib/fn-call.pipe";
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'test-component',
-  template: ''
+  template: '',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TestComponent {
+  static instance: TestComponent;
+
+  constructor() {
+    TestComponent.instance = this;
+  }
+
   @Input()
   private testInput: unknown;
 
@@ -17,17 +24,17 @@ export class TestComponent {
 }
 
 @Component({
-  template: '<test-component #comp [testInput]="[thisUsingFunction, 8] | fnCall"></test-component>'
+  template: '<test-component *ngIf="true" [testInput]="([thisUsingFunction, 8] | fnCall | async) + 3"></test-component>'
 })
 export class TestComponentWrapper {
   @ViewChild('comp')
   component: TestComponent;
 
-  activeFunctionExpression: FunctionExpression<any> = null;
   private internalValue = 5;
 
-  thisUsingFunction(number: number) {
-    return this.internalValue + number;
+  thisUsingFunction(number: number): Observable<number> {
+    expect(this).toBeInstanceOf(TestComponentWrapper);
+    return of(this.internalValue + number);
   }
 }
 
@@ -41,6 +48,6 @@ describe('Multi Level fnCall Pipe', () => {
   it('should correctly infer the this argument', async () => {
     const fixture = TestBed.createComponent(TestComponentWrapper);
     fixture.detectChanges();
-    expect(fixture.componentInstance.component.getValue()).toBe(13);
+    expect(TestComponent.instance.getValue()).toBe(16);
   });
 });
